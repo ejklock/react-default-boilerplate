80f95d91b5215ad25b6219287f804f89bb90d0dd
Evaldo Klock <neto.nemesis@gmail.com>
2020-01-02 15:57:07 -0400

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 80px auto;
+`;
+
+export { Container };
